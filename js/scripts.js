674e8d9148592f61d2e80356db83a97260371a03
Bren <captainbmq@gmail.com>
2019-06-19 13:27:08 -0400

    worked on js manipulation

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,24 @@
 // 1: Set the text of the <h1> element
 
+const listName = document.getElementsByTagName('h1')
+listName[0].textContent = "ur mum gay.";
 
 // 2: Set the color of the <h1> to a different color
-
+listName[0].style.color = "red";
 
 // 3: Set the content of the '.desc' paragraph
 // The content should include at least one HTML tag
-
+const desc = document.querySelector('.desc')
+desc.innerHTML = "<strong>lmao</strong> git bodied";
 
 // 4: Set the class of the <ul> to 'list'
-
+const ulClass = document.querySelector('ul')
+ulClass.setAttribute = "list"
 
 // 5: Create a new list item and add it to the <ul>
-
+const node = document.createElement('li')
+node.innerHTML = "<input> Spaghetti"
+ulClass.appendChild(node)
 
 // 6: Change all <input> elements from text fields to checkboxes
 
